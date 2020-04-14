daa85a691355881cdb8954f89052b95e9feefc6b
jacobmal <jacobmal@umich.edu>
2020-04-14 14:27:35 -0400

    Update call

--- py_sstat.py
+++ py_sstat.py
@@ -2,6 +2,6 @@ from tabulate import tabulate
 import subprocess
 
 if __name__ == '__main__':
-    output = subprocess.run('/opt/slurm/bin/scontrol show nodes --oneliner | head -2', shell=True)
+    output = subprocess.check_output('/opt/slurm/bin/scontrol show nodes --oneliner | head -2', shell=True)
 
     print(output)
