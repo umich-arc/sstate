77ba8e52051c3d56ade10fdfbeefb9b126a8cbb5
jacobmal <jacobmal@umich.edu>
2020-04-14 14:29:25 -0400

    Decode output

--- py_sstat.py
+++ py_sstat.py
@@ -4,4 +4,4 @@ import subprocess
 if __name__ == '__main__':
     output = subprocess.check_output('/opt/slurm/bin/scontrol show nodes --oneliner | head -2', shell=True)
 
-    print(output)
+    print(output.decode())
